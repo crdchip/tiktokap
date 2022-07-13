cceb6bd52e65685062252ec0ef00bb3bd6a189c6
son-ngo081197 <son.ngo081197@stechvn.org>
2022-07-13 10:18:18 +0700

    add

--- lib/modules/Splash/view/splash_screen.dart
+++ lib/modules/Splash/view/splash_screen.dart
@@ -3,11 +3,7 @@ import 'package:get/get.dart';
 import 'package:tiktokap/modules/Splash/controller/splash_controller.dart';
 import 'package:tiktokap/modules/auth/view/auth_screen.dart';
 import 'package:tiktokap/modules/dash_boar/view/dash_boar_screen.dart';
-import 'package:tiktokap/modules/discover/view/discover_screen.dart';
-import 'package:tiktokap/modules/edit_profile/view/edit_profile.dart';
-import 'package:tiktokap/modules/message/view/messager_screen.dart';
-import 'package:tiktokap/modules/profiles/view/profile_screen.dart';
-import 'package:tiktokap/modules/settings/view/setting_screen.dart';
+
 
 class SplashPage extends GetView<SplashController> {
   const SplashPage({Key? key}) : super(key: key);
@@ -15,7 +11,7 @@ class SplashPage extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<SplashController>(builder: (_) {
-      return controller.userStatus == true ? AuthPage() : SettingPage();
+      return controller.userStatus == true ? AuthPage() : DashBoarPage();
     });
   }
 }
